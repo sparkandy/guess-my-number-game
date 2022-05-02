01369300c217e214709b8119d36d9a5a7a4c3ab8
Andyson <andyson@mypatricia.co>
2022-05-02 12:28:08 +0100

    Add Shadows

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,5 @@
 import { 
+            StyleSheet,
             TextInput, 
             View
         } from 'react-native';
@@ -6,7 +7,7 @@ import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen (){
     return (
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
@@ -15,3 +16,21 @@ function StartGameScreen (){
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    //Drop Shadow on Android
+    elevation: 4,
+    //Drop Shadow on IoS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
